f06f5f9f090f7dbdf243d09454de45234e37f540
djinnome <djinnome@gmail.com>
2018-03-01 17:16:31 -0800

    Filled pathway holes, reactions only

--- utilities.lisp
+++ utilities.lisp
@@ -7,3 +7,12 @@
 		do (format t "~A	~A~%"
 			   (get-frame-name (get-slot-value enzrxn 'enzyme))
 			   (get-frame-name (get-slot-value enzrxn 'reaction))))))
+
+(defun print-rxns-of-filled-pathway-holes (filename)
+  (tofile filename
+	  (format t "Reaction~%")
+	  (loop for enzrxn in (get-class-all-instances '|Enzymatic-Reactions|)
+		when (fequal :pathway-hole-filler
+			      (get-slot-value enzrxn 'basis-for-assignment))
+		do (format t "~A~%"
+			   (get-frame-name (get-slot-value enzrxn 'reaction))))))
